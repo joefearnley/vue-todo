09a17a11d3a945e41ec68812ed1150f54265124c
Joe Fearnley <joe.fearnley@gmail.com>
2017-03-08 16:09:20 -0500

    Adding intialization tests

--- test/unit/specs/TodoList.spec.js
+++ test/unit/specs/TodoList.spec.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import TodoList from '@/components/TodoList'
 
-describe('Hello.vue', () => {
+describe('TodoList.vue', () => {
+
+  const Constructor = Vue.extend(TodoList);
+  const vm = new Constructor().$mount();
+
   it('should render the title', () => {
-    const Constructor = Vue.extend(TodoList);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hero-body h1.title').textContent)
-        .to.equal('Todos');
-  })
+    expect(vm.$el.querySelector('.hero-body h1.title').textContent).to.equal('Todos');
+  });
+
+  it('should initialize data', () => {
+      expect(vm.canAddTodo).to.equal(false);
+      expect(vm.todos.length).to.equal(0);
+      expect(vm.newTodoText).to.equal('');
+      expect(vm.todoToEdit).to.be.an('object');
+      expect(vm.showEditForm).to.equal(false);
+  });
 });
