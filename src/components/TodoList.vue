cbfa0d66198bcb48893913eecdabf6c9fcbe3bd1
Joe Fearnley <joe.fearnley@gmail.com>
2017-03-14 21:17:21 -0400

    Removing single Todo component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -27,7 +27,47 @@
       <div class="container">
         <div class="columns is-multiline">
           <div class="column is-one-quarter" v-for="todo in todos">
-            <todo :todo="todo"></todo>
+            <div class="todo">
+              <div class="card">
+                <div class="card-content">
+                  <div class="content">
+                    <h3>{{ todo.title }}</h3>
+                    <small>{{ todo.created_at | moment('MMM Do YYYY') }}</small>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <a class="card-footer-item complete-todo" title="Complete" @click="completeTodo(todo)">
+                    <span class="icon"><i class="fa fa-check"></i></span>
+                  </a>
+                  <a class="card-footer-item edit-todo" title="Edit" @click="editTodo(todo)">
+                    <span class="icon"><i class="fa fa-pencil"></i></span>
+                  </a>
+                  <a class="card-footer-item edit-todo" title="Delete" @click="deleteTodo(todo)">
+                    <span class="icon"><i class="fa fa-times"></i></span>
+                  </a>
+                </footer>
+              </div>
+            </div>
+            <div class="modal" :class="{ 'is-active': showEditForm }" >
+              <div class="modal-background"></div>
+              <input class="input" type="hidden" v-model="todoToEdit">
+              <div class="modal-content">
+                <div class="box">
+                  <p class="control">
+                    <input class="input" type="text" v-model="editTodoText">
+                  </p>
+                  <div class="control is-grouped">
+                    <p class="control">
+                      <button class="button is-primary" @click="saveTodo">Save</button>
+                    </p>
+                    <p class="control">
+                      <button class="button is-light" @click="removeEditModal">Cancel</button>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <button class="modal-close" @click="removeEditModal"></button>
+            </div>
           </div>
           <hr>
         </div>
@@ -58,13 +98,9 @@
 
 <script>
 import axios from 'axios';
-import Todo from './Todo';
 
 export default {
   name: 'todo-list',
-  components: {
-    Todo
-  },
   data () {
     return {
       canAddTodo: false,
@@ -72,6 +108,10 @@ export default {
       completedTodos: [],
       newTodoText: '',
       showCompleted: false,
+      editTodoText: false,
+      todoToEdit: {},
+      editTodoText: '',
+      showEditForm: false,
       apiUrl: 'http://58beac9f4389c312007f4044.mockapi.io/todo'
     }
   },
