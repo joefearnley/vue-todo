98ef8dd0abcd444175193f9551f05f2b292af2dd
Joe Fearnley <joe.fearnley@gmail.com>
2017-03-21 11:22:44 +0000

    Fixing test

--- test/unit/specs/TodoList.spec.js
+++ test/unit/specs/TodoList.spec.js
@@ -4,11 +4,32 @@ import moxios from 'moxios';
 
 describe('The Todo list renders properly', () => {
   const vm = new Vue(TodoList).$mount();
-  vm.todos.push({id: 1, title: 'Todo 3', created_at: new Date(), completed: false });
-  vm.todos.push({ id: 2, title: 'Todo 3', created_at: new Date(), completed: false });
+  vm.todos.push({id: 1, title: 'Todo 1', created_at: new Date(), completed: false });
+  vm.todos.push({ id: 2, title: 'Todo 2', created_at: new Date(), completed: false });
+  vm.todos.push({ id: 2, title: 'Completed Todo 1', created_at: new Date(), completed: true });
+  
+  beforeEach(function () {
+    moxios.install();
+  });
+
+  afterEach(function () {
+    moxios.uninstall();
+  });
 
   it('should render the title', () => {
-    expect(vm.$el.querySelector('h1.todos-title').textContent).to.equal('Todos (2)');
+    expect(vm.$el.querySelector('h1.todos-title').textContent)
+      .to.equal('Todos (2)');
+  });
+  
+  it('should render the todos', () => {
+    const todos = [...vm.$el.querySelectorAll('.todo h3')];
+    expect(todos[0].textContent).to.equal('Todo 1');
+    expect(todos[1].textContent).to.equal('Todo 2');
+  });
+  
+  it('should render the completed todos title', () => {
+    expect(vm.$el.querySelector('h1.completed-todos-title').textContent)
+      .to.equal('Completed Todos (1)');
   });
 });
 
