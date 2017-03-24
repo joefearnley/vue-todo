1ed815ffc6997611ad33e41e9550788ea8393e09
Joe Fearnley <joe.fearnley@gmail.com>
2017-03-24 20:28:13 +0000

    Adding tests to check if titles rendered

--- test/unit/specs/TodoList.spec.js
+++ test/unit/specs/TodoList.spec.js
@@ -135,36 +135,51 @@ describe('Methods', () => {
   });
 });
 
-// describe('The Todo list renders properly', () => {
-//   const vm = new Vue(TodoList).$mount();
-//   vm.todos.push({id: 1, title: 'Todo 1', created_at: new Date(), completed: false });
-//   vm.todos.push({ id: 2, title: 'Todo 2', created_at: new Date(), completed: false });
-//   vm.todos.push({ id: 2, title: 'Completed Todo 1', created_at: new Date(), completed: true });
-  
-//   beforeEach(function () {
-//     moxios.install();
-//   });
+describe('The Todo list works', () => {
+  const vm = new Vue(TodoList).$mount();
 
-//   afterEach(function () {
-//     moxios.uninstall();
-//   });
+  beforeEach(function () {
+    moxios.install();
+  });
 
-//   it('should render the title', () => {
-//     expect(vm.$el.querySelector('h1.todos-title').textContent)
-//       .to.equal('Todos (2)');
-//   });
+  afterEach(function () {
+    moxios.uninstall();
+  });
+
+  it('should render the title', (done) => {
+    moxios.wait(function () {
+      moxios.requests.mostRecent()
+        .respondWith({ status: 200 })
+        .then(function () {
+          const todosTitle = vm.$el.querySelector('h1.todos-title').textContent;
+          const completedTodosTitle = vm.$el.querySelector('h1.completed-todos-title').textContent;
+
+          expect(todosTitle).to.equal('Todos (2)');
+          expect(completedTodosTitle).to.equal('Completed Todos (2)');
+          done();
+      });
+    });
+  });
   
-//   it('should render the todos', () => {
-//     const todos = [...vm.$el.querySelectorAll('.todo h3')];
-//     expect(todos[0].textContent).to.equal('Todo 1');
-//     expect(todos[1].textContent).to.equal('Todo 2');
-//   });
-  
-//   it('should render the completed todos title', () => {
-//     expect(vm.$el.querySelector('h1.completed-todos-title').textContent)
-//       .to.equal('Completed Todos (1)');
-//   });
-// });
+  // it('should render the todos', () => {
+  //   vm.fetchTodos();
+  //   moxios.wait(function () {
+  //     let request = moxios.requests.mostRecent();
+  //     request.respondWith({
+  //       status: 200,
+  //       response: fakeRepsonse
+  //     }).then(function () {
+  //       expect(vm.todos.length).to.equal(2);
+  //       expect(vm.completedTodos.length).to.equal(1);
+  //
+  //       const todos = [...vm.$el.querySelectorAll('.todo h3')];
+  //       expect(todos[0].textContent).to.equal('Todo 1');
+  //       expect(todos[1].textContent).to.equal('Todo 2');
+  //       done();
+  //     });
+  //   });
+  // });
+});
 
 
   // TODO:
